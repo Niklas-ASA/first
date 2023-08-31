1c05a5cf69922ec3a1fba51dab50698d7dd8a9ca
Niklas Andersen <naaa@itu.dk>
2023-08-31 13:28:38 +0200

    Refactor main to print CLI arguments.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,2 @@
-﻿if (args.Length < 3)
-{
-    Console.WriteLine("Usage: dotnet run stuff ...");
-    return;
-}
-
-if (args[0] == "say")
-{
-    var random = new Random();
-    var message = args[1];
-    var frequency = int.Parse(args[2]);
-    foreach (var i in Enumerable.Range(1, frequency))
-    {
-        Console.Write(message + " ");
-        var wait = random.Next(100, 1000);
-        await Task.Delay(TimeSpan.FromMilliseconds(wait));
-    }
-}
+﻿foreach (var arg in args)
+    Console.WriteLine(arg);
