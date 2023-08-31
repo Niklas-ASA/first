83f98b3aabcacb5379f89bdfe5bd857a437b37d5
Niklas Andersen <naaa@itu.dk>
2023-08-31 13:26:17 +0200

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿if (args.Length < 3)
+{
+    Console.WriteLine("Usage: dotnet run stuff ...");
+    return;
+}
+
+if (args[0] == "say")
+{
+    var random = new Random();
+    var message = args[1];
+    var frequency = int.Parse(args[2]);
+    foreach (var i in Enumerable.Range(1, frequency))
+    {
+        Console.Write(message + " ");
+        var wait = random.Next(100, 1000);
+        await Task.Delay(TimeSpan.FromMilliseconds(wait));
+    }
+}
